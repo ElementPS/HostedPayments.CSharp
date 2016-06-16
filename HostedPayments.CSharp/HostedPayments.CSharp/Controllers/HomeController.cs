b8b2f6d0ae1c7f1d410592fb3c3b88dbf2aa831c
Daniel Ourada <daniel.ourada@gmail.com>
2016-06-16 15:30:54 -0700

    initial checkin

---
+++ HostedPayments.CSharp/HostedPayments.CSharp/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HostedCheckout.MVC.CSharp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Buy()
+        {
+            //ViewBag.URL = "https://hc.mercurycert.net/CheckoutIFrame.aspx?ReturnMethod=get&pid=" + resp.PaymentID;
+            return View();
+        }
+
+        public ActionResult Reserve()
+        {
+
+            //ViewBag.URL = "https://hc.mercurycert.net/CheckoutIFrame.aspx?ReturnMethod=get&pid=" + resp.PaymentID;
+            return View();
+        }
+
+        public ActionResult Complete(string PaymentID, string ReturnCode, string ReturnMessage)
+        {
+
+            return View();
+        }
+
+
+    }
+}
---
+++ HostedPayments.CSharp/HostedPayments.CSharp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HostedCheckout.MVC.CSharp.MvcApplication" Language="C#" %>
